e3fb7d9c5f58e3704b3786e6dad46aef3de77fac
aiantorres <aiantorres@users.noreply.github.com>
2023-02-15 19:10:01 +0100

    0.0.3

---
+++ index.js
@@ -0,0 +1,20 @@
+ function instagram() {
+    document.getElementById("ins").onclick;
+    window.location.href = "https://www.instagram.com";
+ }
+ function linkedin() {
+    document.getElementById("lin").onclick;
+    window.location.href = "https://www.instagram.com";
+ }
+ function telegram() {
+    document.getElementById("ins").onclick;
+    window.location.href = "https://www.instagram.com";
+ }
+ function twitter() {
+    document.getElementById("ins").onclick;
+    window.location.href = "https://www.instagram.com";
+ }
+ function github() {
+    document.getElementById("ins").onclick;
+    window.location.href = "https://github.com/aiantorres";
+ }
